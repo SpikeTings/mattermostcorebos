3aa3b558881229945aeb103eb3457303f8e8c26c
Kliv Shapllo <kliv.shapllo@gmail.com>
2020-04-09 15:58:35 +0200

    Integration of `chatwithme` channel, coreBOS commands, ephemeral post etc features

---
+++ server-plugin/hook.go
@@ -0,0 +1,106 @@
+package server_plugin
+
+import (
+	"errors"
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/plugin"
+	"github.com/tsolucio/corebosgowslib"
+	"mattermost-server-plugin/configuration"
+	"mattermost-server-plugin/configuration/language"
+	"mattermost-server-plugin/helpers"
+	"regexp"
+	"strings"
+)
+
+func (p *Plugin) OnConfigurationChange() error {
+	var c configuration.MattermostConfig
+	err := p.API.LoadPluginConfiguration(&c)
+	if err != nil {
+		return err
+	}
+	c.UpdateConfigurations()
+	corebosgowslib.SetURL(configuration.CorebosUrl)
+	return nil
+}
+
+func (p *Plugin) OnActivate() error {
+	teams, err := p.API.GetTeams()
+	if err != nil {
+		return err
+	}
+	if len(teams) == 0 {
+		return errors.New("there are no existing teams")
+	}
+
+	team := teams[0]
+	channel, _ := p.API.GetChannelByNameForTeamName(team.Name, "chatwithme", false)
+	if channel == nil {
+		channel, err = p.API.CreateChannel(&model.Channel{
+			TeamId:      team.Id,
+			Type:        model.CHANNEL_OPEN,
+			DisplayName: "Chat With Me",
+			Name:        "chatwithme",
+			Header:      "The channel used by the mattermost-extend plugin.",
+			Purpose:     "The channel was created by the mattermost-extend plugin to extend the server functionality.",
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *Plugin) MessageWillBePosted(c *plugin.Context, post *model.Post) (*model.Post, string) {
+	r, _ := regexp.Compile("^\\S+")
+	triggerWord := r.FindString(post.Message)
+	if helpers.Contains(configuration.ChatWithMeTriggerWordsEphemeral, triggerWord) {
+		p.SendPostToChatWithMeExtension(post, triggerWord)
+		p.API.SendEphemeralPost(post.UserId, post)
+		post.Message = "Posted Ephemeral Trigger Word"
+	}
+	return post, ""
+}
+
+func (p *Plugin) MessageHasBeenPosted(c *plugin.Context, post *model.Post) {
+
+	//Regular expression used for the replacement logic of incoming and outgoing webhooks
+	r, _ := regexp.Compile("^\\S+")
+	triggerWord := r.FindString(post.Message)
+
+	if helpers.Contains(configuration.ChatWithMeTriggerWords, triggerWord) {
+		p.SendPostToChatWithMeExtension(post, triggerWord)
+	}
+
+	//Regular expression user for special commands like: open, create, edit, list
+	r, _ = regexp.Compile("^#(\\w+) (\\w+)(?: (\\d+))?$")
+	matches := r.FindStringSubmatch(strings.TrimSpace(post.Message))
+
+	if len(matches) > 0 {
+		if action, ok := language.Command[matches[1]]; ok {
+			module := matches[2]
+			broadcast := &model.WebsocketBroadcast{UserId: post.UserId}
+			payloadData := map[string]interface{}{
+				"action": action,
+				"module": module,
+			}
+			if matches[3] != "" {
+				payloadData["id"] = matches[3]
+			}
+			p.API.PublishWebSocketEvent("corebos", payloadData, broadcast)
+		}
+	}
+
+	// Transfer files in coreBOS
+	if len(post.FileIds) > 0 {
+		channel, err := p.API.GetChannel(post.ChannelId)
+		if err != nil {
+			return
+		}
+		team, err := p.API.GetTeam(channel.TeamId)
+		if err != nil {
+			return
+		}
+		p.UploadFilesToCoreBOS(post.FileIds, channel.DisplayName, team.DisplayName)
+
+	}
+}
--- server-plugin/plugin.go
+++ server-plugin/plugin.go
@@ -1,39 +1,78 @@
 package server_plugin
 
 import (
+	"errors"
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/plugin"
-	"github.com/tsolucio/corebosgowslib"
 	"mattermost-server-plugin/configuration"
+	"net/http"
+	"net/url"
 )
 
 type Plugin struct {
 	plugin.MattermostPlugin
 }
 
-func (p *Plugin) MessageHasBeenPosted(c *plugin.Context, post *model.Post) {
-	// Transfer files in coreBOS
-	if len(post.FileIds) > 0 {
-		channel, err := p.API.GetChannel(post.ChannelId)
-		if err != nil {
-			return
-		}
-		team, err := p.API.GetTeam(channel.TeamId)
-		if err != nil {
-			return
-		}
-		p.UploadFilesToCoreBOS(post.FileIds, channel.DisplayName, team.DisplayName)
+func (p *Plugin) SendPostToChatWithMeExtension(post *model.Post, triggerWord string) error {
 
+	cnl, _ := p.API.GetChannel(post.ChannelId)
+	var tname = ""
+	var tdname = ""
+	var cdname = ""
+	if cnl.Type == "D" {
+		user, errr := p.API.GetUser(post.UserId)
+		if errr != nil {
+			return errr
+		}
+		cdname = user.FirstName + user.LastName
+		tname = user.FirstName + "_" + user.LastName
+		tdname = user.FirstName + "_" + user.LastName
+	} else {
+		team, _ := p.API.GetTeam(cnl.TeamId)
+		tname = team.Name
+		tdname = team.DisplayName
+		cdname = cnl.DisplayName
 	}
-}
+	formData := url.Values{
+		"text":         {post.Message},
+		"token":        {configuration.ChatWithMeToken},
+		"trigger_word": {triggerWord},
+		"user_id":      {post.UserId},
+		"channel_id":   {post.ChannelId},
+		"team_id":      {cnl.TeamId},
+		"team_name":    {tname},
+		"team_dname":   {tdname},
+		"chnl_name":    {cnl.Name},
+		"chnl_dname":   {cdname},
+	}
 
-func (p *Plugin) OnConfigurationChange() error {
-	var c configuration.MattermostConfig
-	err := p.API.LoadPluginConfiguration(&c)
+	newPost := &model.Post{
+		UserId:    post.UserId,
+		ChannelId: post.ChannelId,
+		Type:      model.POST_SLACK_ATTACHMENT,
+	}
+	resp, err := http.PostForm(configuration.ChatWithMeExtensionUrl, formData)
+	defer resp.Body.Close()
+
 	if err != nil {
 		return err
 	}
-	c.UpdateConfigurations()
-	corebosgowslib.SetURL(configuration.CorebosUrl)
+
+	incomingWebhookPayload, decodeError := model.IncomingWebhookRequestFromJson(resp.Body)
+	if decodeError != nil {
+		return decodeError
+	}
+
+	if len(incomingWebhookPayload.Text) == 0 && incomingWebhookPayload.Attachments == nil {
+		return errors.New("Wrong response format")
+	}
+
+	if incomingWebhookPayload.Props != nil {
+		newPost.Props = incomingWebhookPayload.Props
+	}
+	newPost.Message = incomingWebhookPayload.Text
+	newPost.AddProp("attachments", incomingWebhookPayload.Attachments)
+
+	p.API.SendEphemeralPost(newPost.UserId, newPost)
 	return nil
 }
